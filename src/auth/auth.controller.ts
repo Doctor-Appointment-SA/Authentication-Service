a4f8fb98aabcd1dc52c7b90cab9e31683143a265
SwiftkeyX <pisanucaraleadkul.pasit@gmail.com>
2025-10-21 20:31:20 +0700

    final

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,21 @@
-import { Body, Controller, Get, Post, Req, UnauthorizedException, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  Res,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
-import { Request } from 'express';
-import { instanceToPlain } from 'class-transformer';
-import * as util from 'node:util';
+import { Request, Response } from 'express';
+import { AttachRefreshTokenToHeader } from './utils/token.helper';
 
 @Controller('auth')
 export class AuthController {
@@ -18,15 +26,24 @@ export class AuthController {
   ) {}
 
   @Post('register')
-  register(@Body() dto: RegisterDto) {
+  async register(@Body() dto: RegisterDto, @Res({passthrough: true}) res:Response) {
     // console.log('DTO plain:', dto);
-    return this.auth.register(dto);
+    const {user, access_token, refresh_token } = await this.auth.register(dto);
+    
+    AttachRefreshTokenToHeader(refresh_token, res);
+
+    return {user, access_token};
   }
 
   @Post('login')
-  login(@Body() dto: LoginDto) {
-    console.log('AuthController login DTO:', instanceToPlain(dto));
-    return this.auth.login(dto.username, dto.password);
+  async login(@Body() dto: LoginDto, @Res({ passthrough: true }) res: Response) {
+    // get user, access token, refresh token data    
+    const {user, access_token, refresh_token } = await this.auth.login(dto.username, dto.password);
+    
+    // attack refresh token to response heaeder
+    AttachRefreshTokenToHeader(refresh_token, res);
+
+    return {user, access_token};
   }
 
   @Get('whoami')
@@ -38,13 +55,39 @@ export class AuthController {
     return data;
   }
 
+  // @Post('refresh')
+  // async refresh(@Body() body: { refresh_token: string }) {
+  //   const payload = await this.jwt.verifyAsync(body.refresh_token, {
+  //     secret: this.cfg.get('JWT_REFRESH_SECRET'),
+  //     ignoreExpiration: true,
+  //   });
+
+  //   return this.auth.refresh(payload.sub, body.refresh_token);
+  // }
+
   @Post('refresh')
-  async refresh(@Body() body: { refresh_token: string }) {
-    const payload = await this.jwt.verifyAsync(body.refresh_token, {
+  async refresh(
+    @Req() req: Request,
+    @Res({ passthrough: true }) res: Response,
+  ) {
+    const refresh_token = req.cookies.refresh_token;
+    if (!refresh_token)
+      throw new UnauthorizedException('Missing refresh_token cookie');
+
+    // validate refresh_token
+    const payload = await this.jwt.verifyAsync(refresh_token, {
       secret: this.cfg.get('JWT_REFRESH_SECRET'),
-      ignoreExpiration: true,
+      ignoreExpiration: false,
     });
-    return this.auth.refresh(payload.sub, body.refresh_token);
+
+    // generate new access token, refresh token
+    const user_id = payload.sub;
+    const { access_token: new_access_token, refresh_token: new_refresh_token } =
+      await this.auth.refresh(user_id, refresh_token);
+
+    // insert refresh_token in the response
+    AttachRefreshTokenToHeader(new_refresh_token, res);
+    return { new_access_token };
   }
 
   @Post('logout')
